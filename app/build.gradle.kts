29eabb55827bf600157abc052a31cec2a6066190
Adalyat Nazirov <aenazirov@gmail.com>
2024-09-03 21:40:39 +0100

    step 2. complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
