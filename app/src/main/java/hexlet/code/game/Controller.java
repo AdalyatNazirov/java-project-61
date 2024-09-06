4d8be8c4b95d727c92ac46ece669d951c9e03cbc
Adalyat Nazirov <aenazirov@gmail.com>
2024-09-06 10:35:53 +0100

    fix checkstyle

--- app/src/main/java/hexlet/code/game/Controller.java
+++ app/src/main/java/hexlet/code/game/Controller.java
@@ -1,27 +1,40 @@
 package hexlet.code.game;
 
 public class Controller {
-    static final int MAX_NUMBER = 1000;
-    static final int MAX_SCORE = 3;
+    public static final int MAX_NUMBER = 1000;
+    public static final int MAX_SCORE = 3;
+
+    private static final int GREET_INDEX = 1;
+    private static final int EVEN_INDEX = 2;
+    private static final int CALC_INDEX = 3;
+    private static final int GCD_INDEX = 4;
+    private static final int PROGRESSION_INDEX = 5;
+    private static final int PRIME_INDEX = 6;
 
     public static int[] listGames() {
-        return new int[]{1, 2, 3, 4, 5, 6};
+        return new int[]{
+                GREET_INDEX,
+                EVEN_INDEX,
+                CALC_INDEX,
+                GCD_INDEX,
+                PROGRESSION_INDEX,
+                PRIME_INDEX};
     }
 
     public static String getGameName(int gameNumber) {
 
         switch (gameNumber) {
-            case 1:
+            case GREET_INDEX:
                 return "Greet";
-            case 2:
+            case EVEN_INDEX:
                 return "Even";
-            case 3:
+            case CALC_INDEX:
                 return "Calc";
-            case 4:
+            case GCD_INDEX:
                 return "GCD";
-            case 5:
+            case PROGRESSION_INDEX:
                 return "Progression";
-            case 6:
+            case PRIME_INDEX:
                 return "Prime";
             default:
                 throw new IllegalArgumentException("Invalid game number: " + gameNumber);
@@ -50,12 +63,12 @@ public class Controller {
     private static Game chooseGame(int gameNumber) {
 
         return switch (gameNumber) {
-            case 1 -> new EmptyGame();
-            case 2 -> new EvenGame();
-            case 3 -> new CalculatorGame();
-            case 4 -> new GcdGame();
-            case 5 -> new ProgressionGame();
-            case 6 -> new PrimeGame();
+            case GREET_INDEX -> new EmptyGame();
+            case EVEN_INDEX -> new EvenGame();
+            case CALC_INDEX -> new CalculatorGame();
+            case GCD_INDEX -> new GcdGame();
+            case PROGRESSION_INDEX -> new ProgressionGame();
+            case PRIME_INDEX -> new PrimeGame();
             default -> throw new IllegalArgumentException("Invalid game number: " + gameNumber);
         };
     }
